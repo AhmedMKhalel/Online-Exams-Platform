a01dfba228f17d8061a5ad7f5615c76f805c433f
AhmedMKhalel <ahmednetdeveloper6@gmail.com>
2023-10-03 20:29:01 +0300

    first commit

---
+++ OnlineExamPlatform/Authentication/SendEmail.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace OnlineExamPlatform.Authentication
+{
+    public class SendEmail
+    {
+        public void Send(string sendTo, string messageSubject, string messageBody)
+        {
+            SmtpClient smtpClient = new SmtpClient();
+            smtpClient.UseDefaultCredentials = false;
+            smtpClient.Credentials = new NetworkCredential("***********", "****************");
+
+
+            MailMessage mailMessage = new MailMessage();
+            mailMessage.From = new MailAddress(""""""""""""""'", "OnXams");
+            mailMessage.To.Add(sendTo);
+            mailMessage.Subject =messageSubject;
+            mailMessage.Body = messageBody;
+            smtpClient.EnableSsl = true;
+            smtpClient.Send(mailMessage);
+        }
+    }
+}
